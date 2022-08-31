8e7f3c14481175c2aa803c39938bcb71a2bb5fcf
JuanC <jc.psp5@gmail.com>
2022-08-31 07:42:18 -0700

    Completed exercises 39 and 40

---
+++ exercises/ch-2/e2-39.cpp
@@ -0,0 +1,6 @@
+struct Foo {} //will result in compile error due to no semicolon
+
+int main()
+{
+    return 0;
+}
---
+++ exercises/ch-2/e2-40.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+struct Sales_data
+{
+    std::string bookNo;
+    unsigned int units_sold = 0;
+    double revenue = 0.0;
+};
+
+int main()
+{
+    return 0;
+}
